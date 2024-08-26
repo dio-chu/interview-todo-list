c443e9e320d191f9068d24b46f1f2d6d6ebac360
Dio Chu[朱逸珉] <dio.chu@3861-macair13.local>
2024-08-26 17:43:11 +0800

    feature:設定固定size

---
+++ src/common/ComponentSize.js
@@ -0,0 +1,18 @@
+import { defineEnum } from "../definition";
+
+/**
+ * ButtonSize
+ * @author dio-chu
+ * @public
+ * @enum
+ * @description Mode Size.  LG | MD | SM
+ * sm: 14,  //font-size 16px, 18px, 22px
+ * md: 18,
+ * lg: 22
+ */
+
+export const ComponentSize = defineEnum({
+  LG: "lg",
+  MD: "md",
+  SM: "sm",
+});
