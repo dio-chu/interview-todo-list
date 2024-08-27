258872bbba98f8a2f50d24a7b0e61a75452714bb
Dio Chu[朱逸珉] <dio.chu@3861-macair13.local>
2024-08-28 00:41:54 +0800

    feature:add search component

---
+++ src/components/search/CommonSearch.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="search-container">
+    <Icon :icon="icon" class="search-icon" />
+    <input
+      type="text"
+      class="search-input"
+      :value="modelValue"
+      :placeholder="placeholder"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Icon from "../icon/Icon.vue";
+
+export default defineComponent({
+  name: "CommonSearch",
+  /**
+   * CommonSearch 组件
+   * @component
+   * @author dio-chu
+   * @description 搜尋組件
+   * @example
+   *
+   */
+  components: {
+    Icon,
+  },
+  props: {
+    modelValue: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+    },
+    icon: {
+      type: String,
+    },
+  },
+  emits: ["update:modelValue"],
+});
+</script>
+
+<style scoped>
+.search-container {
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+  background-color: white;
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 10px 14px;
+}
+
+.search-icon {
+  position: absolute;
+  left: 2%;
+  color: #757575;
+}
+
+.search-input {
+  border: none;
+  outline: none;
+  padding-left: 32px; /* icon空間 */
+  width: 100%;
+  font-size: 14px;
+}
+
+.search-input::placeholder {
+  color: #bdbdbd;
+}
+</style>
---
+++ src/pages/tests/searchTest.vue
@@ -0,0 +1,38 @@
+<template>
+  <Search
+    v-model="searchQuery"
+    placeholder="請輸入面試紀錄關鍵字"
+    :icon="pencilIcon"
+    @update:modelValue="handleSearch"
+  />
+  <Search
+    v-model="searchQuery"
+    :icon="pencilIcon"
+    @update:modelValue="handleSearch"
+  />
+</template>
+
+<script>
+import { ref } from "vue";
+import Search from "../../components/search/CommonSearch.vue";
+import pencilIcon from "../../assets/pencil.svg";
+
+export default {
+  components: {
+    Search,
+  },
+  setup() {
+    const searchQuery = ref("");
+
+    const handleSearch = (value) => {
+      console.log("input:", value);
+    };
+
+    return {
+      pencilIcon,
+      searchQuery,
+      handleSearch,
+    };
+  },
+};
+</script>
