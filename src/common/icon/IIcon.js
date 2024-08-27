82f3a310ca3d0a4f12a4c91fdc1f875dc84eb65e
Dio Chu[朱逸珉] <dio.chu@3861-macair13.local>
2024-08-28 00:24:13 +0800

    feature:更新更新Icon寫法

---
+++ src/common/icon/IIcon.js
@@ -0,0 +1,21 @@
+import { IconSize } from "../../enum/IconSize";
+
+/**
+ * IIcon
+ * @author dio-chu
+ * @public
+ * @interface
+ * @description Icon 組件的基本接口，包含 icon 和 size 屬性
+ */
+export const IIcon = {
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      default: IconSize.MD,
+    },
+  },
+};
