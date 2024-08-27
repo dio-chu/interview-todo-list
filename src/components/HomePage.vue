01a5ff79b2cf7a1462b17b5aa88d6729ae859d12
Dio Chu[朱逸珉] <dio.chu@3861-macair13.local>
2024-08-27 11:05:28 +0800

    chore:預設值修改

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,17 +1,31 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <CommonSelect :options="selectData" v-model="selected" />
+  <CommonSelect :options="filterGroup" v-model="selected2" />
 </template>
-<script setup>
-import { ref } from "vue";
 
-defineProps({
-  msg: String,
-});
+<script>
+import CommonSelect from "../common/select/CommonSelect.vue";
 
-const count = ref(0);
+export default {
+  components: {
+    CommonSelect,
+  },
+  data() {
+    return {
+      selectData: ["全部", "已安排面試", "已有結果"],
+      filterGroup: ["無", "無聲卡", "未錄取", "已錄取"],
+      selected: "全部",
+      selected2: "無",
+    };
+  },
+
+  watch: {
+    selected(newVal) {
+      console.log("Selected value changed:", newVal);
+    },
+    selected2(newVal) {
+      console.log("Selected2 value changed:", newVal);
+    },
+  },
+};
 </script>
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
