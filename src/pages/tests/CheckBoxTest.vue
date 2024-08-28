4aa80cc30a9cbbc76906205fb0b32016510587a0
Dio Chu[朱逸珉] <dio.chu@3861-macair13.local>
2024-08-28 13:47:45 +0800

    chore:rebase main

---
+++ src/pages/tests/CheckBoxTest.vue
@@ -0,0 +1,16 @@
+<template>
+  <Checkbox v-model="checkbox" />
+</template>
+
+<script>
+import Checkbox from "../../components/checkbox/CheckBox.vue";
+
+export default {
+  components: { Checkbox },
+  data() {
+    return {
+      checkbox: false,
+    };
+  },
+};
+</script>
