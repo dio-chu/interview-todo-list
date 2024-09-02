9c14a5d23edec656a1d3703bac01ccb4b0295efc
dio-chu <dio.chu@104.com.tw>
2024-09-02 17:56:32 +0800

    【ITGD-4748】新增List Header 組件
    * feature:checkbox
    
    * feature:add checkbox component
    
    * chore:拉回main分支資料夾調整內容
    
    * chore:rebase main
    
    * feature:add vue-inheriance & jsdoc
    
    * feature:add ListHeader component
    
    * feature:增加排版彈性
    
    * feat(ITGD-4748):PR修改 props名稱
    
    * chore(ITGD-4748):配合listheader修改
    
    * feat:remove ICheckbox Inheritance
    
    ---------
    
    Co-authored-by: Dio Chu[朱逸珉] <dio.chu@3861-macair13.local>

---
+++ src/components/list/header/ListHeader.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="list-header">
+    <div v-if="needShowCheckbox" class="checkbox-column">
+      <Checkbox :isChecked="isChecked" @update:isChecked="updateIsChecked" />
+    </div>
+    <div v-for="header in headers" :key="header.key" class="header-item">
+      {{ header.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+import Checkbox from "../../checkbox/Checkbox.vue";
+
+export default {
+  name: "ListHeader",
+  /**
+   * ListHeader 組件
+   * @author dio-chu
+   * @component
+   * @implement ICheckbox
+   * @description List的列表頭
+   * @example
+   * <ListHeader  :headers="headers"  isCheckbox   v-model="isAllSelected" />
+   */
+  components: {
+    Checkbox,
+  },
+  props: {
+    headers: {
+      type: Array,
+      required: true,
+    },
+    needShowCheckbox: {
+      type: Boolean,
+      default: false,
+    },
+    isChecked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ["update:isChecked"],
+  methods: {
+    updateIsChecked(value) {
+      this.$emit("update:isChecked", value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.list-header {
+  display: table;
+  width: 100%;
+  table-layout: fixed; /* 確保表格布局不因內容變化而改變 */
+  color: #00797b;
+  font-weight: bold;
+}
+
+.checkbox-column,
+.header-item {
+  display: table-cell;
+  padding: 1rem;
+  text-align: center; /* 各欄位至中 */
+}
+</style>
---
+++ src/pages/tests/ListHeaderTest.vue
@@ -0,0 +1,36 @@
+<template>
+  <ListHeader
+    :headers="headers"
+    needShowCheckbox
+    v-model:isChecked="isAllSelected"
+  ></ListHeader>
+</template>
+
+<script>
+import ListHeader from "../../components/list/header/ListHeader.vue";
+
+export default {
+  components: {
+    ListHeader,
+  },
+  data() {
+    return {
+      headers: [
+        { title: "編輯", key: "edit" },
+        { title: "公司名稱", key: "company" },
+        { title: "面試職位", key: "position" },
+        { title: "狀態", key: "status" },
+        { title: "面試日期", key: "interviewDate" },
+        { title: "更新日期", key: "updateDate" },
+      ],
+      isAllSelected: false,
+    };
+  },
+  watch: {
+    isAllSelected(newValue) {
+      // 這裡您可以添加邏輯來處理所有項目的選中狀態
+      console.log("All items selected:", newValue);
+    },
+  },
+};
+</script>
