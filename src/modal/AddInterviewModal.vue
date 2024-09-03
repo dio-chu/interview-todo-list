d42164f590a899c6cd7f32465166fdfbfedaaab4
Dio Chu[朱逸珉] <dio.chu@3861-macair13.local>
2024-09-03 13:47:19 +0800

    feat:新增/編輯/刪除 Modal 切版

---
+++ src/modal/AddInterviewModal.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="form">
+    <div class="form__group">
+      <label class="form__label">公司名稱</label>
+      <input
+        v-model="formData.companyName"
+        :class="{ 'form__input--error': errors.companyName }"
+        class="form__input"
+        placeholder="請輸入公司名稱"
+      />
+    </div>
+
+    <div class="form__group">
+      <label class="form__label" for="position">面試職位</label>
+      <select v-model="formData.position" class="form__input">
+        <option disabled value="">請選擇面試職位</option>
+        <option value="前端工程師">前端工程師</option>
+        <option value="UIUX設計師">UIUX設計師</option>
+        <option value="全端設計師">全端設計師</option>
+      </select>
+    </div>
+
+    <div class="form__group">
+      <label class="form__label" for="interviewDate">面試日期</label>
+      <input v-model="formData.interviewDate" type="date" class="form__input" />
+    </div>
+
+    <span v-if="errors.position" class="form__error-message">
+      {{ errors.position }}
+    </span>
+
+    <CommonButton label="儲存" @click="submitForm" />
+  </div>
+</template>
+
+<script>
+import CommonModal from "../components/modal/CommonModal.vue";
+import CommonButton from "../components/button/CommonButton.vue";
+
+export default {
+  components: { CommonModal, CommonButton },
+
+  data() {
+    return {
+      formData: {
+        companyName: "",
+        position: "",
+        interviewDate: "",
+      },
+      errors: {},
+    };
+  },
+
+  methods: {
+    validateForm() {
+      //表單驗證邏輯 之後移植vuex
+      this.errors = {};
+      this.errors.companyName = !this.formData.companyName.trim()
+        ? "公司名稱是必填項"
+        : /[!@#$%^&*(),.?":{}|<>]/g.test(this.formData.companyName)
+        ? "公司名稱不能包含特殊符號"
+        : "";
+      this.errors.position = !this.formData.position ? "請選擇面試職位" : "";
+      this.errors.interviewDate = !this.formData.interviewDate
+        ? "請選擇面試日期"
+        : "";
+
+      return Object.values(this.errors).every((error) => !error);
+    },
+
+    submitForm() {
+      console.log("Hi");
+      if (this.validateForm()) {
+        this.$emit("form-submitted");
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.form__group {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  margin: 1rem;
+}
+
+.form__label {
+  color: #00797b;
+  font-weight: bold;
+  margin: 0rem 1.5rem;
+  text-align: left;
+  min-width: 15%;
+}
+
+.form__input,
+.form__select {
+  background-color: #f2f2f2;
+  border: none;
+  border-radius: 3px;
+  margin: 0rem 1.5rem;
+  padding: 8px;
+  flex: 1;
+}
+
+.form__input--error {
+  border: 1px solid rgb(229, 68, 68);
+}
+
+.form__error-message {
+  color: rgb(229, 68, 68);
+  font-size: 0.8em;
+  margin-top: 4px;
+}
+</style>
---
+++ src/modal/DeleteInterviewModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="modal-content">
+      <p>刪除的面試紀錄無法恢復</p>
+      <CommonButton size="sm" label="確認" @click="handleDelete" />
+    </div>
+  </div>
+</template>
+
+<script>
+import CommonModal from "../components/modal/CommonModal.vue";
+import CommonButton from "../components/button/CommonButton.vue";
+
+export default {
+  components: {
+    CommonModal,
+    CommonButton,
+  },
+
+  methods: {
+    handleDelete() {
+      this.$emit("item-deleted");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+.modal-content button {
+  margin-top: 1rem;
+}
+</style>
