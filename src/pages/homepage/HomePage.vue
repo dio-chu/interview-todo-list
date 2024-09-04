75e275074fbe2937d6fa59d4b451ce1cbf39eba5
Dio Chu[朱逸珉] <dio.chu@3861-macair13.local>
2024-09-04 13:20:06 +0800

    feat:頁面與container的的vuex串接

--- src/pages/homepage/HomePage.vue
+++ src/pages/homepage/HomePage.vue
@@ -1,12 +1,27 @@
 <template>
+  <AddInterviewModal
+    :isVisible="showAddModal"
+    @close-modal="toggleAddModal(false)"
+    @submit-form="handleFormSubmit"
+  />
+  <EditInterviewModal
+    :isVisible="showEditModal"
+    @close-modal="toggleEditModal(false)"
+    @update-form="handleUpdateForm"
+  />
+  <DeleteInterviewModal
+    :isVisible="showDeleteModal"
+    @close-modal="toggleDeleteModal(false)"
+    @confirm-delete="handleConfirmDelete"
+  />
   <div class="interview-management">
     <div class="interview-managementactions">
       <CommonButton label="新增面試" size="lg" @click="toggleAddModal(true)" />
     </div>
     <div class="interview-managementfilters">
-      <CommonSelect :options="selectData" v-model="selected" />
+      <CommonSelect :options="selectData" v-model="selectFilterModel" />
       <TextField
-        v-model="searchText"
+        v-model="searchTextModel"
         placeholder="請輸入欲搜尋內容"
         :width="300"
         :prependInnerIcon="magnify"
@@ -15,69 +30,130 @@
     <div class="interview-managementlist-container">
       <ListContainer />
     </div>
-    <CommonModal v-model:isVisible="showAddModal" title="新增面試" :width="400">
-      <AddInterviewModal @form-submitted="toggleAddModal(false)" />
-    </CommonModal>
-    <CommonModal
-      v-model:isVisible="showDeleteModal"
-      title="確定刪除？"
-      :width="350"
-    >
-      <DeleteInterviewModal @item-deleted="toggleDeleteModal(false)" />
-    </CommonModal>
-    <CommonModal
-      v-model:isVisible="showEditModal"
-      title="編輯面試"
-      :width="480"
-    >
-      <EditInterviewModal
-        :item="currentEditItem"
-        @form-submitted="toggleEditModal(false)"
-      />
-    </CommonModal>
   </div>
 </template>
 
 <script>
+import { mapState, mapActions, mapGetters } from "vuex";
 import CommonButton from "../../components/button/CommonButton.vue";
 import CommonSelect from "../../components/select/CommonSelect.vue";
 import TextField from "../../components/textfield/TextField.vue";
 import ListContainer from "./containers/ListContainer.vue";
-import CommonModal from "../../components/modal/CommonModal.vue";
-import AddInterviewModal from "./components/modal/AddInterviewModal.vue";
-import DeleteInterviewModal from "./components/modal/DeleteInterviewModal.vue";
-import EditInterviewModal from "./components/modal/EditInterviewModal.vue";
 import magnify from "../../assets/magnify.svg";
 import { SELECT_DATA } from "./constant";
+import AddInterviewModal from "./components/modal/AddInterviewModal.vue";
+import EditInterviewModal from "./components/modal/EditInterviewModal.vue";
+import DeleteInterviewModal from "./components/modal/DeleteInterviewModal.vue";
+import { useVuelidate } from "@vuelidate/core";
+import { required, helpers } from "@vuelidate/validators";
 
 export default {
+  setup() {
+    return { v$: useVuelidate() };
+  },
   components: {
     CommonButton,
-    CommonModal,
     CommonSelect,
     TextField,
     ListContainer,
     AddInterviewModal,
-    DeleteInterviewModal,
     EditInterviewModal,
+    DeleteInterviewModal,
   },
   data() {
     return {
       magnify,
-      selected: "all",
-      searchText: "",
       selectData: SELECT_DATA,
     };
   },
   computed: {
-    ...mapState({
-      showAddModal: (state) => state.interview.showAddModal,
-      showDeleteModal: (state) => state.interview.showDeleteModal,
-      showEditModal: (state) => state.interview.showEditModal,
-    }),
+    ...mapState("modal", ["showAddModal", "showDeleteModal", "showEditModal"]),
+    ...mapState("interview", ["selectFilter", "searchText"]),
+    ...mapGetters("interview", ["anyItemSelected"]),
+    selectFilterModel: {
+      get() {
+        return this.selectFilter;
+      },
+      set(value) {
+        this.setSelectFilter(value);
+      },
+    },
+    searchTextModel: {
+      get() {
+        return this.searchText;
+      },
+      set(value) {
+        this.setSearchText(value);
+      },
+    },
   },
+
   methods: {
-    ...mapActions(["toggleAddModal", "toggleDeleteModal", "toggleEditModal"]),
+    ...mapActions("modal", [
+      "toggleAddModal",
+      "toggleDeleteModal",
+      "toggleEditModal",
+    ]),
+    ...mapActions("interview", ["setSelectFilter", "setSearchText"]),
+    handleFormSubmit(formData) {
+      this.formData = formData;
+      this.v$.$validate();
+      if (!this.v$.$error) {
+        this.$store.commit("interview/clearErrors");
+        this.$store.dispatch("interview/addInterview", this.formData);
+        this.toggleAddModal(false);
+      } else {
+        this.processValidationErrors();
+      }
+    },
+    handleUpdateForm(updatedData) {
+      this.formData = updatedData;
+      this.v$.$validate();
+      if (!this.v$.$error) {
+        this.$store.commit("interview/clearErrors");
+        this.$store.dispatch("interview/updateInterview", this.formData);
+        this.toggleEditModal(false);
+      } else {
+        this.processValidationErrors();
+      }
+    },
+    processValidationErrors() {
+      const errors = {};
+      if (this.v$.formData.company.$error) {
+        errors.company = this.v$.formData.company.$errors[0].$message;
+      }
+      if (this.v$.formData.position.$error) {
+        errors.position = this.v$.formData.position.$errors[0].$message;
+      }
+      if (this.v$.formData.interviewDate.$error) {
+        errors.interviewDate =
+          this.v$.formData.interviewDate.$errors[0].$message;
+      }
+      this.$store.commit("interview/setErrors", errors);
+    },
+    handleConfirmDelete() {
+      this.$store.dispatch("interview/deleteSelectedItems");
+    },
+  },
+  validations() {
+    return {
+      formData: {
+        company: {
+          required: helpers.withMessage("公司名稱不能為空", required),
+
+          trimWhitespace: helpers.withMessage(
+            "公司名稱不能包含前後空白",
+            (value) => value.trim() === value
+          ),
+        },
+        position: {
+          required: helpers.withMessage("面試職位不能為空", required),
+        },
+        interviewDate: {
+          required: helpers.withMessage("面試日期不能為空", required),
+        },
+      },
+    };
   },
 };
 </script>
